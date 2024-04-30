d65bf0f3cbd903dc93eae49103189f594a07864c
Isabella Jorgensen <isabellamj139@gmail.com>
2024-04-30 15:26:45 +0000

    purchase_cost

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,5 @@
-SELECT *
+SELECT 
+SUM(CAST(purchase_price as float64)* CAST(quantity as float64)) AS purchase_cost_cal
     FROM {{ ref('stg_raw_data__sales') }} as sales
     INNER JOIN {{ ref('stg_raw_data__product') }} as product
     ON sales.products_id=product.products_id
