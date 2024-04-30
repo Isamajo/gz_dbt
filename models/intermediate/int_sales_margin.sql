0c43946602b9d3d315d177c49eab6c58e2121ab0
Isabella Jorgensen <isabellamj139@gmail.com>
2024-04-30 14:10:04 +0000

    added JOIN & margin calculations

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,4 @@
 SELECT *
-FROM gz_raw_data.raw_gz_sales AS sales
-INNER JOIN gz_raw_data.raw_gz_ship AS ship
+FROM {{ ref('stg_raw_data__sales') }} as sales
+INNER JOIN {{ ref('stg_raw_data__ship') }} as ship
   ON sales.orders_id=ship.orders_id
