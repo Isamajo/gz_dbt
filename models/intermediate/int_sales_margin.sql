ccb9b60f4a3e6b45f12d95001967b8c9c5d13c0e
Isabella Jorgensen <isabellamj139@gmail.com>
2024-04-30 15:02:47 +0000

    new changes

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,4 @@
-WITH sales_ship_comb as
-
-(SELECT
-FROM {{ ref('stg_raw_data__sales') }} as sales
-INNER JOIN {{ ref('stg_raw_data__ship') }} as ship
-  ON sales.orders_id=ship.orders_id)
-
-
 SELECT *
-    ,SUM(purchase_price*quantity) AS purchase_cost
-    SUM(revenue-purchase_cost) AS margin
-FROM sales_ship_comb
+    FROM {{ ref('stg_raw_data__sales') }} as sales
+    INNER JOIN {{ ref('stg_raw_data__product') }} as product
+    ON sales.products_id=product.products_id
