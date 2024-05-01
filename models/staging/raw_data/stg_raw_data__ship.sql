d73cafad62ed0b80c0eebc6f8b25f243d45bcf0a
Isabella Jorgensen <isabellamj139@gmail.com>
2024-05-01 07:59:48 +0000

    added float to all

--- models/staging/raw_data/stg_raw_data__ship.sql
+++ models/staging/raw_data/stg_raw_data__ship.sql
@@ -10,9 +10,9 @@ renamed as (
 
     select
         orders_id,
-        shipping_fee,
-        logcost,
-        ship_cost
+        CAST(shipping_fee as float64) as shipping_fee,
+        CAST(logcost as float64) as logcost,
+        CAST(ship_cost as float64) as ship_cost
 
     from source
 
