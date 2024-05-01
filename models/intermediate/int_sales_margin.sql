c04e095692f0c9b81d9d040854bc9d51ce50c9af
Isabella Jorgensen <isabellamj139@gmail.com>
2024-05-01 07:16:05 +0000

    margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,12 @@
 SELECT 
-    ROUND (SUM(CAST(purchase_price as float64)* CAST(quantity as float64)),2) AS purchase_cost_cal
-    FROM {{ ref('stg_raw_data__sales') }} as sales
-    INNER JOIN {{ ref('stg_raw_data__product') }} as product
-    ON sales.products_id=product.products_id
-    GROUP BY orders_id
-
-
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("stg_raw_data__sales")}} s
+LEFT JOIN {{ref("stg_raw_data__product")}} p 
+    USING (products_id)
