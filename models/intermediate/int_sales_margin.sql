274a9c6d40deea4055e51e365f5b38720669d2fb
Isabella Jorgensen <isabellamj139@gmail.com>
2024-05-01 07:23:58 +0000

    cleaned up code

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,10 +3,9 @@ SELECT
     date_date, 
     orders_id,
     revenue, 
-    quantity, 
-    CAST(purchase_price AS FLOAT64), 
+    quantity,  
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    ROUND(s.revenue - (s.quantity*CAST(p.purchase_price AS FLOAT64)),2) AS margin
 FROM {{ref("stg_raw_data__sales")}} s
 LEFT JOIN {{ref("stg_raw_data__product")}} p 
     USING (products_id)
